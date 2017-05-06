5c7422d86e78007466a0f6121bcfb9de21b13160
Gabe Martin <gabema@gmail.com>
2017-05-06 13:22:15 -0700

    Moved evaluate option and made pieces bigger

--- src/components/GuessRow.js
+++ src/components/GuessRow.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Piece from './Piece';
 import Pin from './Pin';
 import './GuessRow.css';
+import guess from '../../public/guess.png';
 
 const colorValue = ['black', 'white', 'red'];
 
@@ -12,10 +13,21 @@ const pieceHandler = (inPlay, onPieceClicked, piece, index) => {
             () => {};
 };
 
-const showEvaluateOption = (inPlay, index, onEvaluateRow) => {
-    return !inPlay ? [] :
-            [<div onClick={() => onEvaluateRow(index)} key="1">Evaluate</div>];
-};
+const showPinGroup = (inPlay, index, onEvaluateRow, pins) => 
+    inPlay ? 
+    <div className='evalute-group' onClick={() => onEvaluateRow(index)}><img className="evalute-image" alt="Evaluate" src={guess} /></div> :
+    <div className='pin-group'>
+        <div className='pin-row'>
+            <Pin color={colorValue[pins[0] || 0]}/>
+            <Pin color={colorValue[pins[1] || 0]}/>
+        </div>
+        <br/>
+        <div className='pin-row'>
+            <Pin color={colorValue[pins[2] || 0]}/>
+            <Pin color={colorValue[pins[3] || 0]}/>
+        </div>
+    </div>
+;
 
 const GuessRow = ({piece1, piece2, piece3, piece4, index, pins, inPlay, onPieceClicked, onEvaluateRow}) => (
       <div className='guess-row'>
@@ -25,18 +37,7 @@ const GuessRow = ({piece1, piece2, piece3, piece4, index, pins, inPlay, onPieceC
             <Piece color={piece3} onClick={pieceHandler(inPlay, onPieceClicked, 'piece3', index)} />
             <Piece color={piece4} onClick={pieceHandler(inPlay, onPieceClicked, 'piece4', index)} />
           </div>
-          <div className='pin-group'>
-            <div className='pin-row'>
-                <Pin color={colorValue[pins[0] || 0]}/>
-                <Pin color={colorValue[pins[1] || 0]}/>
-            </div>
-            <br/>
-            <div className='pin-row'>
-                <Pin color={colorValue[pins[2] || 0]}/>
-                <Pin color={colorValue[pins[3] || 0]}/>
-            </div>
-          </div>
-          {showEvaluateOption(inPlay, index, onEvaluateRow)}
+          {showPinGroup(inPlay, index, onEvaluateRow, pins)}
       </div>
 );
 
